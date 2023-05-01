d97fc634af59deff000e5b5b863853805075031f
Douglas-i <hdouglasu@gmail.com>
2023-05-01 09:45:35 -0600

    Creacion del Header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './assets/css/componentes/card.css'
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
 import Page404 from './pages/Page404'
+import Header from './Components/Header'
 
 import  {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
@@ -21,6 +22,7 @@ function App() {
 
   return (    
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/sobre' element={<Sobre />}/>
