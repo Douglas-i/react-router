c7b1b7ae1315b96ba69f0a4724a637aeb09ad997
Douglas-i <hdouglasu@gmail.com>
2023-05-01 09:08:17 -0600

    Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+
+import  {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <>
 
-    </>
+  /*const location = window.location.pathname;
+  console.log(location);
+
+  const Router = () => {
+    if(location === '/'){
+      return <Home />
+    } else {
+      return <Sobre />
+    }
+  }*/
+
+  return (    
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/sobre' element={<Sobre />}/>
+      </Routes>
+    </Router>
   );
-}
 
+}
 export default App;
